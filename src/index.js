e8b9ee03bc00b0b8f3f8f92955ce26fbfafc791e
Sive <siveloveslean@gmail.com>
2025-09-15 17:22:07 +0200

    feat: Create content of homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./styles.css";
+
+const content = document.getElementById("content");
+
+const welcomeText = document.createElement("p");
+welcomeText.setAttribute("id", "welcome-text");
+welcomeText.textContent = "WELCOME TO SEAR";
+
+const horizontal = document.createElement("hr");
+
+const headlineText = document.createElement("h1");
+headlineText.setAttribute("id", "headline-text");
+headlineText.textContent = "Meat. Fire. Mastery.";
+
+const findATableBtn = document.createElement("button");
+findATableBtn.setAttribute("id", "find-a-table");
+findATableBtn.textContent = "Find a Table";
+
+content.appendChild(welcomeText);
+content.appendChild(horizontal);
+content.appendChild(headlineText);
+content.appendChild(findATableBtn);
