3919326584fcf6f82ec2ccefa3cc83cb825409f7
Sive <siveloveslean@gmail.com>
2025-09-15 17:26:21 +0200

    refactor: Create function to load content

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
 import "./styles.css";
+import { loadContent } from "./on-page-load";
 
-const content = document.getElementById("content");
-
-const welcomeText = document.createElement("p");
-welcomeText.setAttribute("id", "welcome-text");
-welcomeText.textContent = "WELCOME TO SEAR";
-
-const horizontal = document.createElement("hr");
-
-const headlineText = document.createElement("h1");
-headlineText.setAttribute("id", "headline-text");
-headlineText.textContent = "Meat. Fire. Mastery.";
-
-const findATableBtn = document.createElement("button");
-findATableBtn.setAttribute("id", "find-a-table");
-findATableBtn.textContent = "Find a Table";
-
-content.appendChild(welcomeText);
-content.appendChild(horizontal);
-content.appendChild(headlineText);
-content.appendChild(findATableBtn);
+loadContent();
