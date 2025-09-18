735d1ce4b146560c8ab6fc1cf391f3a7e1964f42
Sive <siveloveslean@gmail.com>
2025-09-18 18:50:14 +0200

    feat: Style the about tab

--- src/about-page.js
+++ src/about-page.js
@@ -4,27 +4,43 @@ export function loadAboutPage() {
 
   const content = document.getElementById("content");
   content.innerHTML = "";
+
+  const aboutContent = document.createElement("div");
+  aboutContent.setAttribute("id", "about-content");
   
   const storyContainer = document.createElement("div");
   storyContainer.setAttribute("id", "story-container");
 
   const ourStoryTitle = document.createElement("h1");
+  ourStoryTitle.setAttribute("id", "story-title");
   ourStoryTitle.textContent = "Our Story";
 
   const ourStoryText = document.createElement("p");
-  ourStoryText.textContent = "Lorem ipsum dolor sit amet.";
+  ourStoryText.setAttribute("id", "story-text");
+  ourStoryText.textContent = `At the heart of our kitchen is fire—ancient, elemental, 
+  and alive. From the very beginning, we set out to honor the craft of flame grilling, 
+  pairing time-honored techniques with the refinement of modern fine dining.\n Every
+  cut of meat we serve is carefully selected, prepared with respect, and kissed by open 
+  flame until it reaches perfection. It’s not just cooking—it’s a ritual of smoke, heat, 
+  and patience that turns simple ingredients into unforgettable meals.`;
 
-  const imageContainer = document.createElement("div");
+  const imageContainer = document.createElement("figure");
   imageContainer.setAttribute("id", "image-container");
+  const imageCaption = document.createElement("figcaption");
+  imageCaption.textContent = "Sear's Pit & Flame, in New York";
 
   const image = document.createElement("img");
+  image.setAttribute("id", "about-image");
   image.src = aboutPageImage;
 
   storyContainer.appendChild(ourStoryTitle);
   storyContainer.appendChild(ourStoryText);
-
+  
   imageContainer.appendChild(image);
+  imageContainer.appendChild(imageCaption);
 
-  content.appendChild(storyContainer);
-  content.appendChild(imageContainer);
+  aboutContent.appendChild(storyContainer);
+  aboutContent.appendChild(imageContainer);
+
+  content.appendChild(aboutContent);
 }
